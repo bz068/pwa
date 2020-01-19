b7be96d2a8a58afc705f087f06f357d824dcaaff
bz068 <bz068@live.mdx.ac.uk>
2020-01-19 06:50:58 +0000

    commit

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var template = "<article>\n\
-	<img src='data/img/placeholder.png' data-src='data/img/course.jpg' alt='NAME'>\n\
-	<h3>#POS. TOPIC</h3>\n\
-	<ul>\n\
+<h3>#POS. TOPIC</h3>\n\
 	<li><span>Topic:</span> <strong>TOPIC</strong></li>\n\
     <li><span>Location:</span> <strong>LOCATION</strong></li>\n\
     <li><span>Price:</span> <strong>PRICE</strong></li>\n\
@@ -10,10 +8,11 @@ var template = "<article>\n\
 var content = '';
 for(var i=0; i<courses.length; i++) {
 	var entry = template.replace(/POS/g,(i+1))
-				.replace(/TOPIC/g,data[i].topic)
-                .replace(/LOCATION/g,data[i].location)
-                .replace(/PRICE/g,data[i].price);
+				.replace(/TOPIC/g,courses[i].topic)
+                .replace(/LOCATION/g,courses[i].location)
+                .replace(/PRICE/g,courses[i].price);
 entry = entry.replace('<a href=\'http:///\'></a>','-');
 	content += entry;
 };
-document.getElementById('content').innerHTML = content;
+document.getElementById('content').innerHTML = content; 
+
