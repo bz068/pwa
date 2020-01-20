fecedb28de1ba6ca8940ccab4587b2dae02199b8
bz068 <bz068@live.mdx.ac.uk>
2020-01-20 23:07:53 +0000

    data

--- data/courses.js
+++ data/courses.js
@@ -1,7 +1,18 @@
 var courses = 
 [
-    { 'topic': 'math', 'location': 'hendon', 'price': 100 },
-    { 'topic': 'math', 'location': 'colindale', 'price': 80 },
-    { 'topic': 'math', 'location': 'brent cross', 'price': 90 },
-    { 'topic': 'math', 'location': 'golders green', 'price': 120 },
+    { topic: 'math',
+     location: 'hendon',
+      price: 100 },
+
+    { topic: 'math',
+     location: 'colindale',
+      price: 80 },
+
+    { topic: 'math',
+     location: 'brent cross',
+      price: 90 },
+      
+    { topic: 'math',
+     location: 'golders green'
+     , price: 120 },
    ];
