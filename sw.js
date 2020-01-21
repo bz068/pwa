9edd5a6b94146a27303ae3e177d41e25a5eab447
bz068 <bz068@live.mdx.ac.uk>
2020-01-21 00:39:05 +0000

    com2

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 
 // Installing Service Worker
 self.addEventListener('install', function(e) {
-    console.log('[Service Worker] Install');
+    console.log('[ServiceWorker] Installed');
     e.waitUntil(
       caches.open(cacheName).then(function(cache) {
         console.log('[Service Worker] Caching all: app shell and content');
